89042d7def4d52fc406fd802f4139302ef0b6630
wehao <30787186+wehaox@users.noreply.github.com>
2021-09-09 14:29:50 +0800

    Delete getver.js

--- js/getver.js
+++
@@ -1,7 +0,0 @@
-        $(document).ready(function() {
-            $.get("http://typecho.wehao.ml", function(data) {
-                console.log(data);
-                $("#latest").text(data.ver);
-            });
-
-        });
