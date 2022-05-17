cc15f94f932bf46a2f09931039ac741063834d23
wehao <30787186+wehaox@users.noreply.github.com>
2022-05-17 20:57:04 +0800

    1.5.3update
    1.5.3update

--- js/themecustom.js
+++ js/themecustom.js
@@ -1,22 +1,37 @@
 $('.set_toc').click(function(){ 
-    $('.mtoc').slideToggle("fast");
+    $('.mtoc').slideToggle("fast")
 });
 if (screen.width>=1200){
    $(".mtoc a").click(function() {
-        var hre = $(this).attr("href");
+        var hre = $(this).attr("href")
         $('html, body').animate({
-                scrollTop: $(hre).offset().top-110}, 300);
-});
+                scrollTop: $(hre).offset().top-110}, 300)
+})
 }else if (screen.width>=768 && screen.width<990){
    $(".mtoc a").click(function() {
-        var hre = $(this).attr("href");
+        var hre = $(this).attr("href")
         $('html, body').animate({
-                scrollTop: $(hre).offset().top-120}, 300);
-}); 
+                scrollTop: $(hre).offset().top-120}, 300)
+})
 }else if(screen.width<768){
 $(".mtoc a").click(function() {
         var hre = $(this).attr("href");
         $('html, body').animate({
-                scrollTop: $(hre).offset().top-160}, 300);
-});
+                scrollTop: $(hre).offset().top-190}, 300)
+})
+}
+
+const hash = window.location.hash
+if (hash != '') {
+    $('.mtoc').slideToggle("fast")
+}
+window.onload=function(){
+    const point = localStorage.getItem("point")
+    if(point !== null){
+       document.getElementById("point").href = point
+    }
+    document.querySelector(".typecho-option-submit > li > button").addEventListener("click", function(){
+        const hash = window.location.hash
+        localStorage.setItem("point", hash)
+    });
 }
